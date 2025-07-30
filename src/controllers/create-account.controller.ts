7e4f254cb015291c88ab6684dc1e1a9ebcecb2f5
Paulo Borges <dev.pauloborges@gmail.com>
2025-07-30 11:00:46 -0300

    add: zod validation pipe; add zod and zod-validation-error

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -7,6 +7,15 @@ import {
 } from '@nestjs/common'
 import { hash } from 'bcryptjs'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -14,7 +23,7 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
